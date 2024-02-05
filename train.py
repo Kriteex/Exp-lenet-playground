5ef0bed100065b03431661b742f89d85ce846015
Kriteex <42377553+Kriteex@users.noreply.github.com>
2024-02-05 14:04:38 +0100

    Add files via upload
    Meta learning (kind of) included

--- train.py
+++ train.py
@@ -4,6 +4,50 @@ from dataset import load_data
 import torch.nn.functional as F
 import torch.nn as nn
 from utils import hinge_loss
+import torch.optim as optim
+import copy
+import random
+
+def evaluate_adapted_model(model, device, data, target):
+    model.eval()
+    with torch.no_grad():
+        data, target = data.to(device), target.to(device)
+        output = model(data)
+        loss = F.cross_entropy(output, target).item()
+    return loss
+
+def train_maml(model, device, train_loader, optimizer, epoch, number_of_adaptation_steps_max=50, alpha=0.01):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+
+        adapted_models = [copy.deepcopy(model) for _ in range(3)]
+        losses = []
+        for i, adapted_model in enumerate(adapted_models):
+            adapted_optimizer = optim.SGD(adapted_model.parameters(), lr=alpha)
+            number_of_adaptation_steps = random.randint(0, number_of_adaptation_steps_max)
+
+            # Provádějte více kroků adaptace na stejném mini-batchi
+            for adaptation_step in range(number_of_adaptation_steps):  # Lets suppose: number_of_adaptation_steps > 1
+                adapted_optimizer.zero_grad()
+                output = adapted_model(data.to(device))
+                loss = F.cross_entropy(output, target.to(device))
+                loss.backward()
+                adapted_optimizer.step()
+
+            # Evaluate the adapted model on the same mini-batch
+            loss = evaluate_adapted_model(adapted_model, device, data, target)
+            losses.append((loss, i))
+            
+        # Select the model that learned the most (has the lowest loss)
+        _, best_model_idx = min(losses)
+
+        # Use the best model's parameters to update the original model
+        best_model = adapted_models[best_model_idx]
+        model.load_state_dict(best_model.state_dict())
+
+        if batch_idx % 1 == 0:
+            print(f'Batch: {batch_idx}, Selected Model: {best_model_idx}')
+
 
 def train(model, device, train_loader, optimizer, epoch):
     model.train()
@@ -60,10 +104,14 @@ def main():
 
     # Trénování modelu
     for epoch in range(1, 2):  # Trénování pro 10 epoch
-        train(model, device, train_loader, optimizer, epoch)
+        #train(model, device, train_loader, optimizer, epoch)
+        train_maml(model, device, train_loader, optimizer, epoch)
+    
 
     # Testování modelu
     test(model, device, test_loader)
    
 if __name__ == "__main__":
     main()
+
+
